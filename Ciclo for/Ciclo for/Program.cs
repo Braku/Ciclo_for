22b5e5ae4aad78d8b6a79f0630a9c502504f22da
Braku <barukbumer@gmail.com>
2019-08-24 18:16:18 -0500

    Hice el ciclo

---
+++ Ciclo for/Ciclo for/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ciclo_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Ingresa el numero con el que quieres que inicie el ciclo:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa el numero con el que quieres que finalice el ciclo:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa el numero con el que quieres que tu ciclo avance:");
+            z = Convert.ToInt32(Console.ReadLine());
+            for (int i = x; i <= y; i+=z)
+            {
+                Console.WriteLine("i = " + i);
+            }
+        }
+    }
+}
